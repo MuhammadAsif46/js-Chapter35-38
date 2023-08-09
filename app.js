50ce00170bb355bbdf9a80a30fe075ac9b93b759
MuhammadAsif46 <asifahmed32c@gmail.com>
2023-08-10 00:32:08 +0500

    task 2 completed

--- app.js
+++ app.js
@@ -4,3 +4,15 @@
 //     document.write(new Date());
 // }
 // date();
+
+
+
+//************ Question 2 ************//
+
+// function greet(){
+//     var firstName = prompt("enter first name");
+//     var lastName = prompt("enter last name");
+//     var fullName = firstName  + " " + lastName;
+//     document.write(fullName);
+// }
+// greet();
